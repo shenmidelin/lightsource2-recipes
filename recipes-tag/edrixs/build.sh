7975a3fae1595041312a04815eadff7163c5aa89
Maksim Rakitin <mrakitin@bnl.gov>
2019-06-28 19:49:50 +0000

    WIP version of edrixs

---
+++ recipes-tag/edrixs/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+pwd
+ls -alF
+
+make -C src F90=mpif90 LIBS="-lopenblas -lparpack -larpack"
+make install -C src
+
+# build python part of edrixs
+$PYTHON setup.py build_ext --library-dirs=$LIBRARY_LIB
+$PYTHON -m pip install . --no-deps --ignore-installed -vvv
