d5ce3ad1b5d282be3e7df63f91de5b4cfb74a5d0
Yilin Wang <wangyilin2015@gmail.com>
2019-07-01 20:49:33 +0000

    update build.sh

--- recipes-tag/edrixs/build.sh
+++ recipes-tag/edrixs/build.sh
@@ -7,5 +7,5 @@ make -C src F90=mpif90 LIBS="-lopenblas -lparpack -larpack"
 #make install -C src
 
 # build python part of edrixs
-$PYTHON setup.py config_fc build_ext --library-dirs=$LIBRARY_LIB
+$PYTHON setup.py build_ext --library-dirs=$LIBRARY_LIB
 $PYTHON -m pip install . --no-deps --ignore-installed -vvv
